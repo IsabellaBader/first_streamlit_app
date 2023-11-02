489e8285fa123b39f48fd3df349f03361d6a6aca
IsabellaBader <149376352+IsabellaBader@users.noreply.github.com>
2023-11-02 14:15:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -41,7 +40,7 @@ try:
     streamlit.dataframe(back_from_function)
 
 #import snowflake.connector
-
+streamlit.header ("The fruit load list contains")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
